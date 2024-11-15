f81f2fed749fd695d04c0836fa4f4b4bdb07dc4c
Sampat Sarkar <sampat7123@gmail.com>
2024-11-15 19:50:42 +0530

    Initial commit

---
+++ 05_modules/test.py
@@ -0,0 +1,30 @@
+from packages.maths import *
+from packages.subpackages.mult import *
+print(addition(2,3))
+print(sub(2,3))
+print(multiply(4,5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ m.py
@@ -0,0 +1,8 @@
+# lst1=[1,2,3,4]
+# lst2=["a","b","c","d"]
+# pair=list(zip(lst1,lst2))
+# print(pair)
+
+# numbers=[1,2,3,4,5,6,7,8,9]
+
+# list(map(square,numbers))
